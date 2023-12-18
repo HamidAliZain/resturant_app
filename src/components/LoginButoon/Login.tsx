ad47f644297a84f31e7c14143c9756c51f7d4c23
HAmid <hamidprince823@gmail.com>
2023-12-18 20:17:14 +0500

    auth config

---
+++ src/components/LoginButoon/Login.tsx
@@ -0,0 +1,67 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useUser } from "@auth0/nextjs-auth0/client";
+const LoginButton = ({ openLoginBar }: { openLoginBar: any }) => {
+  const { user } = useUser();
+  let imageOfLogin: any = user?.picture?.toString();
+  console.log(openLoginBar);
+  return (
+    <>
+      {!user && (
+        <div
+          className={
+            openLoginBar
+              ? "absolute top-20 right-12 w-64   bg-white z-10  flex justify-between"
+              : "hidden"
+          }
+        >
+          <Link
+            href={"/api/auth/login"}
+            className="w-1/2 text-center bg-secondary text-white py-1 hover:bg-white hover:text-black transition-all ease-linear"
+          >
+            <div className="w-full px-2 transition-all ease-linear ">
+              <p className="text-lg">login</p>{" "}
+            </div>
+          </Link>
+          <Link
+            href={"/api/auth/login"}
+            className="w-1/2 text-center bg-priamry text-white py-1 hover:bg-white hover:text-black transition-all ease-linear"
+          >
+            <div className="w-full px-2 transition-all ease-linear ">
+              <p className="text-lg">create</p>{" "}
+            </div>
+          </Link>
+        </div>
+      )}
+      {user && (
+        <div
+          className={
+            openLoginBar
+              ? "absolute top-20 right-12 w-64   bg-white z-10 "
+              : "hidden"
+          }
+        >
+          <img
+            src={imageOfLogin}
+            alt=""
+            width={80}
+            height={80}
+            className="rounded-full mt-9 mx-auto"
+          />
+
+          <div className="text-xl  w-full px-2 hover:bg-gray-300 transition-all ease-linear mt-8">
+            <p className="text-lg">{user.name}</p>{" "}
+          </div>
+          <Link href={"/api/auth/logout"}>
+            <div className="w-full px-2 hover:bg-gray-300 transition-all ease-linear ">
+              <p className="text-lg">logout</p>{" "}
+            </div>
+          </Link>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default LoginButton;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,18 +6,19 @@ import logo from "../../public/logo.png";
 import { CgProfile } from "react-icons/cg";
 import Link from "next/link";
 import { CartContext } from "@/components/context";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import LoginButton from "./LoginButoon/Login";
 const Navbar = () => {
   const { cart } = useContext(CartContext);
-  return (
-    <header>
+  const [openLoginBar, setOpenLoginBar] = useState(false);
+  console.log(openLoginBar);return (
+    <header className="relative">
       <nav className="w-full flex justify-between px-6 pt-2  ">
         <Link href="/" className="">
           <Image src={logo} alt="logo" className=" cursor-pointer  w-24 ml-4" />
         </Link>
         <div className="w-1/2 flex justify-end items-center gap-x-6   ">
           <Link href="/cart" className="relative">
-            {" "}
             <div
               className={
                 cart.length <= 0
@@ -28,10 +29,14 @@ const Navbar = () => {
               {cart.length}
             </div>
             <Image src={Cart} alt="cart" className="w-6 " />
-          </Link>{" "}
-          <CgProfile className="text-3xl " />
+          </Link>
+          <CgProfile
+            className="text-2xl"
+            onClick={() => setOpenLoginBar(!openLoginBar)}
+          />
         </div>
       </nav>
+      <LoginButton openLoginBar={openLoginBar} />
     </header>
   );
 };
